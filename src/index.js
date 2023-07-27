6a3899490a5f96677199e4c50bc4fc888ad37854
Viettq <taquocviet262@gmail.com>
2023-07-27 23:12:16 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -30,6 +30,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 //127.0.0.1
-              app.listen(port, () => {
-                console.log(`Example app listening on port http://localhost:${port}`);
-              });
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
