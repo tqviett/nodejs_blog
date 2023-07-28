a471cc8dcc3f9d5cf22066f35f29afbc6d6ce500
Viettq <taquocviet262@gmail.com>
2023-07-29 01:20:05 +0700

    Create new course

--- src/app/models/courseModel.js
+++ src/app/models/courseModel.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-  name: { type: String, default: '' },
-  description: { type: String, default: '' },
-  image: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+  {
+    name: { type: String, default: '' },
+    description: { type: String, default: '' },
+    image: { type: String },
+    videoId: { type: String },
+    level: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 module.exports = mongoose.model('Course', Course);
--- src/routes/coursesRoute.js
+++ src/routes/coursesRoute.js
@@ -4,7 +4,8 @@ var route = express.Router();
 const CoursesController = require('../app/controllers/coursesController');
 
 // newsController.index
-
+route.get('/create', CoursesController.create);
+route.post('/store', CoursesController.store);
 route.get('/:slug', CoursesController.show);
 
 module.exports = route;
